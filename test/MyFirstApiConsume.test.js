94d63a2249e2a60c05fb1361a72e1301bd1be02d
Stefany Mojica <stefymojica40@gmail.com>
2022-06-29 18:33:38 -0500

    Adding expects

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -26,20 +26,6 @@ describe('First Api Tests', () => {
         expect(response.config.query).to.eql(query);
     });
 
-    //Consumiendo servicios HEAD
-    it('Consume HEAD Service', async () => {
-        const response = await axios.head('https://httpbin.org/headers');
-
-        expect(response.status).to.equal(StatusCodes.OK)
-    });
-
-    //Consumiendo servicios DELETE
-    it('Consume DELETE Service', async () => {
-        const response = await axios.delete('https://httpbin.org/delete');
-
-        expect(response.status).to.equal(StatusCodes.OK);
-    });
-
     //Consumiendo servicios POST
     it('Consume POST Service', async () => {
         const body = {
@@ -47,10 +33,39 @@ describe('First Api Tests', () => {
             age: '56',
             city: 'New York'
         }
-        const response = await axios.post('https://httpbin.org/post', { body });
+        const response = await axios.post('https://httpbin.org/post', body );
+
         expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.data.json).to.eql(body);
+
+        //expect(response.data.json.name).to.eql('Fred');
+        //expect(response.data.json.age).to.eql('56');
+        //expect(response.data.json.city).to.eql('New York');
     });
 
+    //Consumiendo servicios HEAD
+    it('Consume HEAD Service', async () => {
+        const response = await axios.head('https://httpbin.org/headers');
+
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.data).to.eql('');
+    });
+
+    //Consumiendo servicios DELETE
+    it('Consume DELETE Service', async () => {
+        const body = {
+            name: 'Fred',
+            age: '56',
+            city: 'New York'
+        }
+        const response = await axios.delete('https://httpbin.org/delete', body);
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.data.json).to.eql(null);
+
+        //console.log(response)
+    
+    });
+    
     //Consumiendo servicios PATCH
     it('Consume PATCH Service', async () => {
         const body = {
@@ -58,8 +73,10 @@ describe('First Api Tests', () => {
             age: '56',
             city: 'New York'
         }
-        const response = await axios.patch('https://httpbin.org/patch', { body });
+        const response = await axios.patch('https://httpbin.org/patch', body);
         expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.data.json).to.eql(body);
+
     });
 
     //Consumiendo servicios PUT
@@ -69,13 +86,11 @@ describe('First Api Tests', () => {
             age: '56',
             city: 'New York'
         }
-        const response = await axios.put('https://httpbin.org/put', { body });
+        const response = await axios.put('https://httpbin.org/put', body);
         expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.data.json).to.eql(body);
     });
 });
-
-
-
 
 
 
