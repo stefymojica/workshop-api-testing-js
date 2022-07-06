3fcd77cd1b044ff534838ad349401372428a1826
Stefany Mojica <71105508+stefymojica@users.noreply.github.com>
2022-07-06 09:36:13 -0500

    GitHub authentication

---
+++ test/GithubApi.Authentication.test.js
@@ -0,0 +1,33 @@
+const { StatusCodes } = require('http-status-codes');
+const { expect } = require('chai');
+const axios = require('axios');
+
+const urlBase = 'https://api.github.com';
+const githubUserName = 'stefymojica';
+const repository = 'workshop-api-testing-js';
+
+describe('Github Api Test', () => {
+  describe('Authentication', () => {
+    it('Via OAuth2 Tokens by Header', async () => {
+      const response = await axios.get(`${urlBase}/repos/${githubUserName}/${repository}`, {
+        headers: {
+          Authorization: `${process.env.ACCESS_TOKEN}`
+        }
+      });
+
+      expect(response.status).to.equal(StatusCodes.OK);
+      expect(response.data.description).equal('This is a Workshop about API Testing in JavaScript');
+    });
+  });
+
+  // prueba autenticacion por parametros
+  it('Via OAuth2 Tokens by parameter', async () => {
+    const response = await axios.get(
+      `${urlBase}/repos/${githubUserName}/${repository}`,
+      { access_token: process.env.ACCESS_TOKEN }
+    );
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.description).equal('This is a Workshop about API Testing in JavaScript');
+  });
+});
