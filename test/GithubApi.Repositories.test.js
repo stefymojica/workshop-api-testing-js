27c15438ddec7eeaea8141a46c644c5ea7da1e0d
Stefany Mojica <stefymojica40@gmail.com>
2022-07-07 10:16:02 -0500

    Deleting authentication Github Api test

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -2,26 +2,6 @@ const axios = require('axios');
 const { expect } = require('chai');
 const chai = require('chai');
 chai.use(require('chai-subset'));
-const { StatusCodes } = require('http-status-codes');
-
-const urlBase = 'https://api.github.com';
-const githubUserName = 'stefymojica';
-const repository = 'workshop-api-testing-js';
-
-describe('Github Api Test', () => {
-  describe('Authentication', () => {
-    it('Via OAuth2 Tokens by Header', async () => {
-      const response = await axios.get(`${urlBase}/repos/${githubUserName}/${repository}`, {
-        headers: {
-          Authorization: `${process.env.ACCESS_TOKEN}`
-        }
-      });
-
-      expect(response.status).to.equal(StatusCodes.OK);
-      expect(response.data.description).equal('This is a Workshop about API Testing in JavaScript');
-    });
-  });
-});
 
 describe('Api Github', () => {
   it('Consume GET service', async () => {
